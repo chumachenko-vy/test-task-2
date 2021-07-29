0c6300fac4190018fd82962ca446fb95d6cd36c1
Ubuntu <ubuntu@ip-172-31-29-182.ec2.internal>
2021-07-29 14:17:49 +0000

    second commit

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var http = require("http");
 var fs = require('fs');
+const format = require("node.date-time");
+const winston = require('winston');
 var port = 8080;
 var serverUrl = "0.0.0.0";
 var counter = 0;
@@ -13,15 +15,14 @@ var server = http.createServer(function(req, res) {
 
     fs.readFile("sample.html", function(err, text){
       res.setHeader("Content-Type", "text/html");
-      res.end(text);
+      res.end(text)
     });
     return;
-
   }
 
   res.setHeader("Content-Type", "text/html");
   res.end("<p>Test task for Chumachenko Volodymyr (PDFfiller) 07/29/2021.    Request counter: " + counter + ".</p>");
-
+  fs.appendFile('counter.log',+counter+'\n');
 });
 
 console.log("Starting web server at " + serverUrl + ":" + port);
